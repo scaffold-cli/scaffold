1ce1a59874b5db67495e39cbaea23f16a4430fd7
Rob Cabacungan <robspassky@gmail.com>
2019-06-10 09:09:35 -0400

    Log to a file instead of console

--- src/main/java/scaffoldcli/scaffold/ScaffoldApplication.java
+++ src/main/java/scaffoldcli/scaffold/ScaffoldApplication.java
@@ -1,13 +1,25 @@
 package scaffoldcli.scaffold;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScaffoldApplication {
+public class ScaffoldApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(ScaffoldApplication.class);
 
 	public static void main(String[] args) {
+	    // LOG.info("STARTING THE APPLICATION");
 		SpringApplication.run(ScaffoldApplication.class, args);
+		// LOG.info("APPLICATION FINISHED");
+	}
+
+	@Override
+	public void run(String... args) {
+		LOG.info("EXECUTING THE run function");
 	}
 
 }
--- src/test/java/scaffoldcli/scaffold/ScaffoldApplicationTests.java
+++ src/test/java/scaffoldcli/scaffold/ScaffoldApplicationTests.java
@@ -1,16 +1,27 @@
 package scaffoldcli.scaffold;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ScaffoldApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testRunMethod() {
+		ScaffoldApplication app = new ScaffoldApplication();
+		app.run();
+	}
+
+	@Test
+	public void testMainMethod() {
+		ScaffoldApplication.main(new String[0]);
 	}
 
 }
